b341407b521f31b418571a9c22e1301a62702577
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-07-21 13:02:02 +0700

    cleaning

--- Head/src/HForm/WikiModelTest.java
+++
@@ -1,7 +0,0 @@
-package HForm;
-
-public class WikiModelTest {
-    public static void main(String[] args) {
-        System.out.println(WikipediaAPIRequest.APISearchRequest("Hán Binh Đế"));
-    }
-}
